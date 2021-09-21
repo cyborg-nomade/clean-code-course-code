164e2b578c71a474db24616f6dfef03cb2848be7
Uriel Fiori <uriel.fiori@gmail.com>
2021-09-21 16:44:13 -0300

    corrections

--- 01-challenge-problem.py
+++ 01-challenge-problem.py
@@ -20,10 +20,14 @@ class Rectangle:
         print('Starting Point (Y)): ' + str(self.origin.y))
         print('End Point X-Axis (Top Right): ' + str(top_right))
         print('End Point Y-Axis (Bottom Left): ' + str(bottom_left))
+    
+def build_rectangle():
+    origin_point = Point(50, 100)
+    rectangle = Rectangle(origin_point, 90, 10)
 
+    return rectangle
 
-origin_point = Point(50, 100)
-rectangle = Rectangle(origin_point, 90, 10)
+rectangle = build_rectangle()
 
 print(rectangle.area())
 rectangle.print_end_points()
